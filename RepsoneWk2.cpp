20821f643daf965eb2df4c73160fff163bea4166
John <johntbryant0@gmail.com>
2026-03-28 20:39:29 -0600

    Response to Lane Dorscher
    For this code, I found and corrected the following errors:
    1.  Lines 11 and 12 the string name; and int age where move inside of the int main() funticon and a semi colon was added to the end of  int age
    
    2.  Line 22 a < was missing for "<< name <"  now written as "<<name<<"
    
    3. A return 0; line was added at the end of function before final bracket

---
+++ RepsoneWk2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int main()
+{
+	string name;
+	int age
+
+	cout << "Hello World! What is your name? \n";
+	cin >> name;
+
+	cout << "How old are you? \n";
+	cin >> age;
+
+	cout << "Hello " << name << ", you are " << age << " years old! \n";
+
+	return 0;
+
+}
